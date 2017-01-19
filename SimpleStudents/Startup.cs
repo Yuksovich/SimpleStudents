5f11e412b70cfdfd16e821beed8ada27d8468683
Yuksovich <yuksiph@gmail.com>
2017-01-19 11:11:37 +0700

    Добавьте файлы проекта.

---
+++ SimpleStudents/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleStudents.Startup))]
+namespace SimpleStudents
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
