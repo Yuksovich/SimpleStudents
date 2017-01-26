d7ca1cf7c8a19ea1430ade383c35a2fd9417b814
Yuksovich <yuksiph@gmail.com>
2017-01-26 18:26:42 +0700

    Added seeder to seed Database with initial data

--- Data/Infrastructure/UniversitySeeder.cs
+++ Data/Infrastructure/UniversitySeeder.cs
@@ -8,18 +8,65 @@ namespace Data.Infrastructure
     {
         protected override void Seed(UniversityContext context)
         {
-            List<Course> coursesList = new List<Course>()
+
+            var teacher1 = new Teacher() {FirstName = "Vasya", LastName = "Vasin"};
+            var teacher2 = new Teacher() {FirstName = "Ivan", LastName = "Ivanov"};
+            var teacher3 = new Teacher() {FirstName = "Petr", LastName = "Petrov"};
+            var course1 = new Course() {Name = "Java"};
+            var course2 = new Course() {Name = "C#"};
+            var course3 = new Course() {Name = "Visual Basic"};
+            var teacherCourse11 = new TeacherCourse() {Course = course1, Teacher = teacher1};
+            var teacherCourse22 = new TeacherCourse() {Course = course2, Teacher = teacher2};
+            var teacherCourse33 = new TeacherCourse() {Course = course3, Teacher = teacher3};
+            var teacherCourse31 = new TeacherCourse() {Course = course3, Teacher = teacher1};
+            var teacherCourse23 = new TeacherCourse() {Course = course2, Teacher = teacher3};
+            
+            course1.TeacherCourse = new List<TeacherCourse>()
             {
-                new Course() {Name = "Physics"},
-                new Course() {Name = "Math" },
-                new Course() {Name = "English" }
+                teacherCourse11
             };
-            foreach (var course in coursesList)
+            course2.TeacherCourse = new List<TeacherCourse>()
             {
-                context.Courses.Add(course);
-            }
-            context.SaveChanges();
-                base.Seed(context);
+                teacherCourse22,
+                teacherCourse23
+            };
+            course3.TeacherCourse = new List<TeacherCourse>()
+            {
+                teacherCourse31,
+                teacherCourse33
+            };
+
+            teacher1.TeacherCourses = new List<TeacherCourse>()
+            {
+                teacherCourse31,
+                teacherCourse11
+            };
+
+            teacher2.TeacherCourses = new List<TeacherCourse>()
+            {
+                teacherCourse22
+            };
+
+            teacher3.TeacherCourses = new List<TeacherCourse>()
+            {
+                teacherCourse23,
+                teacherCourse33
+            };
+
+            context.Teachers.Add(teacher1);
+            context.Teachers.Add(teacher2);
+            context.Teachers.Add(teacher3);
+            context.Courses.Add(course1);
+            context.Courses.Add(course2);
+            context.Courses.Add(course3);
+            context.TeacherCourses.Add(teacherCourse11);
+            context.TeacherCourses.Add(teacherCourse22);
+            context.TeacherCourses.Add(teacherCourse33);
+            context.TeacherCourses.Add(teacherCourse31);
+            context.TeacherCourses.Add(teacherCourse23);
+
+          context.SaveChanges();
+            base.Seed(context);
         }
     }
 }
