72b87dd1f5406f0eaf956fc0ba391d11b82460fd
Yuksovich <yuksiph@gmail.com>
2017-01-26 19:09:44 +0700

    courses minor corrections

--- Data/Repositories/CourseRepository.cs
+++ Data/Repositories/CourseRepository.cs
@@ -1,28 +1,30 @@
 ﻿using System.Collections.Generic;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using Data.Infrastructure;
-using SimpleStudents;
 using SimpleStudents.Domain;
 
 namespace Data.Repositories
 {
     public interface ICourseRepository : IRepository<Course>
     {
-       
     }
 
     public class CourseRepository : RepositoryBase<Course>, ICourseRepository
     {
-        public CourseRepository(IDbFactory dbFactory) : base(dbFactory) { }
+        public CourseRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
 
         public override IEnumerable<Course> GetAll()
         {
             var context = Context as UniversityContext;
-            context.Database.Log = (s => System.Diagnostics.Debug.WriteLine(s));
-
-
-            return context?.Courses.Include(c => c.TeacherCourse).Include(i=>i.TeacherCourse.Select(s=>s.Teacher)).ToList();
+            
+            return
+                context?.Courses.Include(c => c.TeacherCourse)
+                    .Include(i => i.TeacherCourse.Select(s => s.Teacher))
+                    .ToList();
         }
     }
 }
