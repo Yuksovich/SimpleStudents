dc1d3691aa2fc1e8c9d3b353899ec40199fb2647
Yuriy <yuksiph@gmail.com>
2017-02-02 17:16:23 +0700

    Minor code formatting

--- SimpleStudents/Models/UserModel.cs
+++ SimpleStudents/Models/UserModel.cs
@@ -5,9 +5,10 @@ namespace SimpleStudents.Web.Models
     public abstract class UserModel
     {
         public int Id { get; set; }
-        [Required, MaxLength(255)]
+
+        [Required, MaxLength(255), Display(Name = "First name")]
         public string FirstName { get; set; }
-        [Required, MaxLength(255)]
+        [Required, MaxLength(255), Display(Name = "Last name")]
         public string LastName { get; set; }
     }
 }
