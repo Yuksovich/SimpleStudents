0cc66bfbbdfdbbac276c1c5066ce5503cd59c3ee
Yuriy <yuksiph@gmail.com>
2017-02-08 15:23:57 +0700

    Added WebApiController to receive students e-mails

---
+++ SimpleStudents/Api/StudentsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using SimpleStudents.Data.Infrastructure;
+using SimpleStudents.Data.Repositories;
+
+namespace SimpleStudents.Web.Api
+{
+    public class StudentsController : ApiController
+    {
+        private IStudentsRepository Students { get; }
+
+        public StudentsController(IStudentsRepository students)
+        {
+            Students = students;
+        }
+
+        //GET api/students
+        public IEnumerable<string> GetEmails()
+        {
+            return Students.GetAll().Select(s => s.Email).ToList();
+        }
+    }
+}
--- SimpleStudents/Startup.cs
+++ SimpleStudents/Startup.cs
@@ -1,6 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
 using Autofac;
 using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
 using Microsoft.Owin;
 using Owin;
 using SimpleStudents.Data.Infrastructure;
@@ -17,8 +20,9 @@ namespace SimpleStudents.Web
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-
+            var config = GlobalConfiguration.Configuration;
             var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
             builder.RegisterControllers(typeof(MvcApplication).Assembly).PropertiesAutowired();
             builder.RegisterSource(new ViewRegistrationSource());
             builder.RegisterType<UniversityDbFactory>().As<IDbFactory>().InstancePerRequest();
@@ -27,10 +31,11 @@ namespace SimpleStudents.Web
             builder.RegisterType<StudentsRepository>().As<IStudentsRepository>().InstancePerRequest();
             builder.RegisterType<TeacherCourseRepository>().As<ITeacherCourseRepository>().InstancePerRequest();
             builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
-
+            
             builder.RegisterType<TeacherCourseServices>().As<ITeachersCourseServices>().InstancePerRequest();
            
             var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
             app.UseAutofacMiddleware(container);
             app.UseAutofacMvc();
