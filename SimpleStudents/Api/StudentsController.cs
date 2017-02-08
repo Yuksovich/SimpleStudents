c03a0329b2b2d19c6b62c257d1afa23e5e8ab3c5
Yuriy <yuksiph@gmail.com>
2017-02-08 17:16:08 +0700

    Added custom validation (not finished)

--- SimpleStudents/Api/StudentsController.cs
+++ SimpleStudents/Api/StudentsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using SimpleStudents.Data.Infrastructure;
@@ -20,6 +21,11 @@ namespace SimpleStudents.Web.Api
         //GET api/students
         public IEnumerable<string> GetEmails()
         {
+            var r = new Random().Next(100);
+            if (r < 30)
+            {
+                Thread.Sleep(2000);
+            }
             return Students.GetAll().Select(s => s.Email).ToList();
         }
     }
