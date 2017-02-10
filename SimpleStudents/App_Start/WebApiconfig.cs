68e7c550a134097fb765d96fd7408a749669a4c3
Yuriy <yuksiph@gmail.com>
2017-02-10 13:30:09 +0700

    Code cleanup

--- SimpleStudents/App_Start/WebApiconfig.cs
+++ SimpleStudents/App_Start/WebApiconfig.cs
@@ -14,7 +14,7 @@ namespace SimpleStudents.Web
             config.Routes.MapHttpRoute(
                 name: "Custom",
                 routeTemplate: "api/{controller}/{action}/{id}",
-                defaults: new {id = RouteParameter.Optional}
+                defaults: new { id = RouteParameter.Optional }
                 );
 
             var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
